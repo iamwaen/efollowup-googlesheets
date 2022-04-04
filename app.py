01e446c0dd1da7104ed996a769bedc37973deac2
Waraporn Chantabarn <iamwaen@gmail.com>
2022-04-04 17:24:59 +0700

    change sheet id and range name

--- app.py
+++ app.py
@@ -15,13 +15,13 @@ def home():
     creds = None
     creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-    SPREADSHEET_ID = '1ry4S9dDev1K2cS4_yYXV_Gfx4_QSGJjDsnwaGoXbsCk'
-    RANGE_NAME = 'Sheet1!A1:G'
+    SPREADSHEET_ID = '1BoSNCRqZmFZ6r4bp2T2p88VbLBd-OjBoqUzfEo1iPhY'
+    RANGE_NAME = 'Summary_Result!A1:M'
 
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,  range=RANGE_NAME).execute()
-    values = result.get('values')
+    values = result.get('values',[])
 
     return render_template('index.html', len = len(values), listdata = values)
 
