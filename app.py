e051f31be19243ab6d1c6372f555e101f420378a
Waraporn Chantabarn <iamwaen@gmail.com>
2022-04-05 16:16:59 +0700

    get chart value

--- app.py
+++ app.py
@@ -27,7 +27,21 @@ def home():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    SERVICE_ACCOUNT_FILE = 'credentials/key-sheets-api.json'
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+    creds = None
+    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+    SPREADSHEET_ID = '1BoSNCRqZmFZ6r4bp2T2p88VbLBd-OjBoqUzfEo1iPhY'
+    RANGE_NAME = 'Sheet7!B1:R3'
+
+    service = build('sheets', 'v4', credentials=creds)
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,  range=RANGE_NAME).execute()
+    values = result.get('values')
+    
+    return render_template('dashboard.html', len = len(values), listdata = values)
 
 if __name__ == '__main__':
     app.run(debug=True)
