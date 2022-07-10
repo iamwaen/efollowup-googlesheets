ff1c1cb98424876b31711f00af078c8da3282144
Waraporn Chantabarn <52275355+iamwaen@users.noreply.github.com>
2022-07-10 18:44:45 +0700

    Get mutiple sheets and merge multiple lists

--- app.py
+++ app.py
@@ -6,24 +6,36 @@ from google.oauth2 import service_account
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
 
+@app.route('/')
 def home():
     SERVICE_ACCOUNT_FILE = 'credentials/key-sheets-api.json'
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
     creds = None
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-    SPREADSHEET_ID = 'SPREADSHEET_ID'
+    SPREADSHEET_ID = '1MNuPdQ1NwTOGRiW3pOZQ_2tO_XYIldrc2iyCz_2wmnA'
     RANGE_NAME = 'Summary_Result!A1:M'
 
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,  range=RANGE_NAME).execute()
-    values = result.get('values',[])
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+    values = result.get('values', [])
 
-    return render_template('index.html', len = len(values), listdata = values)
+    SPREADSHEET_ID2 = '1fE0B7jRrV0gpr2EN5l3Cdc2PAmR-P0OYQ6gwkueAl28'
+    RANGE_NAME2 = 'Summary_Result!A2:M'
+    service2 = build('sheets', 'v4', credentials=creds)
+    sheet2 = service2.spreadsheets()
+    result2 = sheet2.values().get(spreadsheetId=SPREADSHEET_ID2,
+                                  range=RANGE_NAME2).execute()
+    values2 = result2.get('values', [])
+
+    values.extend(values2)
+
+    return render_template('index.html', len=len(values), listdata=values)
+
 
 @app.route('/dashboard')
 def dashboard():
@@ -31,17 +43,19 @@ def dashboard():
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
     creds = None
-    creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-    SPREADSHEET_ID = 'SPREADSHEET_ID'
+    SPREADSHEET_ID = '1MNuPdQ1NwTOGRiW3pOZQ_2tO_XYIldrc2iyCz_2wmnA'
     RANGE_NAME = 'Dashboard!B1:R12'
 
     service = build('sheets', 'v4', credentials=creds)
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,  range=RANGE_NAME).execute()
-    values = result.get('values',[])
-    
-    return render_template('dashboard.html', len = len(values), listdata = values)
+    values = result.get('values', [])
+
+    return render_template('dashboard.html', len=len(values), listdata=values)
+
 
 if __name__ == '__main__':
     # app.run(debug=True)
--- readsheets.py
+++ readsheets.py
@@ -7,15 +7,27 @@ SERVICE_ACCOUNT_FILE = 'credentials/key-sheets-api.json'
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 creds = None
-creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 
-SPREADSHEET_ID = '1BoSNCRqZmFZ6r4bp2T2p88VbLBd-OjBoqUzfEo1iPhY'
+SPREADSHEET_ID = '1MNuPdQ1NwTOGRiW3pOZQ_2tO_XYIldrc2iyCz_2wmnA'
 RANGE_NAME = 'Summary_Result!A1:M'
 
 service = build('sheets', 'v4', credentials=creds)
 
 sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,  range=RANGE_NAME).execute()
+result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
 values = result.get('values', [])
 
+SPREADSHEET_ID2 = '1fE0B7jRrV0gpr2EN5l3Cdc2PAmR-P0OYQ6gwkueAl28'
+RANGE_NAME2 = 'Summary_Result!A2:M'
+service2 = build('sheets', 'v4', credentials=creds)
+sheet2 = service2.spreadsheets()
+result2 = sheet2.values().get(spreadsheetId=SPREADSHEET_ID2,
+                              range=RANGE_NAME2).execute()
+values2 = result2.get('values', [])
+
+values.extend(values2)
+
 print(values)
+# print(values2)
